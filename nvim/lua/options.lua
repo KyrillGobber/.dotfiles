4386127ae36470d62e5cc0a62fe8fac66696059c
Kyrill <Kyrill.gobber@cleoag.ch>
2023-01-31 17:04:35 +0100

    Starting new lua stuff

---
+++ nvim/lua/options.lua
@@ -0,0 +1,19 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- use spaces for tabs and whatnot
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.cmd [[ set noswapfile ]]
