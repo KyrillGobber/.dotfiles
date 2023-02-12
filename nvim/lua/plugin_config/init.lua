69ce164e7d84b026ea6cede1638f4f09a02f20fe
Kyrill <Kyrill.gobber@cleoag.ch>
2023-02-12 20:11:16 +0100

    Almost completely migrated to lua now

--- nvim/lua/plugin_config/init.lua
+++ nvim/lua/plugin_config/init.lua
@@ -1,2 +1,5 @@
 require("plugin_config.theme")
 require("plugin_config.nvim-tree")
+require("plugin_config.lualine")
+require("plugin_config.coc")
+require("plugin_config.telescope")
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -22,8 +22,33 @@ return require('packer').startup(function(use)
         },
         tag = 'nightly' -- optional, updated every week. (see issue #1193)
     }
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+    use {'neoclide/coc.nvim', branch = 'release'}
+    use {'Yggdroot/indentLine'}
+    use {'tpope/vim-commentary'}
+    use {'tpope/vim-fugitive'}
+    use {'Raimondi/delimitMate'}
+    use {'mhinz/vim-signify'}
+    use {'sindrets/diffview.nvim'}
+    use {'nvim-lua/popup.nvim'}
+    use {'nvim-lua/plenary.nvim'}
+    use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.1',
+      requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use {'fannheyward/telescope-coc.nvim'}
+    use {
+        'rmagatti/session-lens',
+        requires = {'rmagatti/auto-session'},
+        config = function()
+            require('session-lens').setup({path_display={'shorten'},})
+        end
+    }
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
   if packer_bootstrap then
     require('packer').sync()
   end
