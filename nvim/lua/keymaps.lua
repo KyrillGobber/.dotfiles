8895f1156a28a2cfe9795138dc0880f9f2578cb4
Kyrill <kyrill.gobber@cleoag.ch>
2023-02-12 13:37:23 +0100

    More lua stuff

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,9 @@
+-- Maintain visual mode after shifting with > and <
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+vim.keymap.set('n', '<C-Tab>', ':bnext<cr>')
+vim.keymap.set('n', '<S-C-Tab>', ':bNext<cr>')
+vim.keymap.set('n', '<F4>', ':bd<cr>')
+
+--nnoremap <leader>p :Prettier<CR>
+--nnoremap <leader>ss :source ~/.config/nvim/init.vim<cr>
