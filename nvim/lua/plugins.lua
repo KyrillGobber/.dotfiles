a9c8bc4096987deb8d74a3fbdd498772f03ec1dc
Kyrill <Kyrill.gobber@cleoag.ch>
2023-04-19 17:39:22 +0200

    Added copilot

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -89,6 +89,7 @@ return require('packer').startup(function(use)
             })
         end
     }
+    use {'github/copilot.vim'}
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
   if packer_bootstrap then
