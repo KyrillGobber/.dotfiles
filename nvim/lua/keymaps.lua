8dd369bbbe67302c9ee844d281a59df98e131903
Kyrill <Kyrill.gobber@cleoag.ch>
2023-07-12 21:52:48 +0200

    Some cool new binds, thanks prime

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,6 +1,30 @@
 -- Maintain visual mode after shifting with > and <
 vim.keymap.set('v', '<', '<gv')
 vim.keymap.set('v', '>', '>gv')
+-- Move selected line / block of text in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- Centered half page scroll
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Keep search centered
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+-- copy to system clipboard: asbjornHaland
+-- vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+-- vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- quickfix navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Search and replace word under cursor
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
 vim.keymap.set('n', '<C-Tab>', ':bnext<cr>')
 vim.keymap.set('n', '<S-C-Tab>', ':bNext<cr>')
 vim.keymap.set('n', '<F4>', ':bd<cr>')
