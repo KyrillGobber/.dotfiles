7ffb64c5dccb4fc069d8dcb569c40940795d519e
Kyrill <kyrill@gobber.ch>
2023-10-17 10:27:31 +0200

    feat: Hyprland and waybar changes

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -18,7 +18,7 @@ return require('packer').startup(function(use)
     -- Core
     use { "kelly-lin/ranger.nvim" }
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
     use 'nvim-treesitter/nvim-treesitter'
