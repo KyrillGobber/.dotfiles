a30d8d9066dcb0151e2574bf24700beee94e293f
Kyrill <kyrill@gobber.ch>
2025-04-05 19:14:55 +0200

    feat: starting new install script, some hypr adjustments

--- config/.config/nvim/lua/plugin_config/codecompanion.lua
+++ config/.config/nvim/lua/plugin_config/codecompanion.lua
@@ -32,7 +32,7 @@ return {
 						},
 						schema = {
 							model = {
-								default = "qwen2.5-coder-32b",
+								default = "deepseek-r1-671b",
 								-- Other models available:
 								--   llama-3.1-405b
 								--   llama-3.2-3b
