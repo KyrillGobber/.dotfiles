3ec4efb5615f5149d016666354c9065823afff52
Kyrill <kyrill@gobber.ch>
2025-04-09 19:24:14 +0200

    treesitter things

--- config/.config/nvim/lua/plugin_config/treesitter.lua
+++ config/.config/nvim/lua/plugin_config/treesitter.lua
@@ -1,30 +1,30 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/nvim-treesitter",
     config = function()
-        require'nvim-treesitter.configs'.setup {
-        -- A list of parser names, or "all" (the four listed parsers should always be installed)
-        ensure_installed = { "c", "lua", "typescript", "tsx", "javascript", "http", "json", "vim", "markdown", "markdown_inline"},
+        require 'nvim-treesitter.configs'.setup {
+            -- A list of parser names, or "all" (the four listed parsers should always be installed)
+            ensure_installed = { "c", "lua", "typescript", "tsx", "javascript", "http", "json", "vim", "markdown", "markdown_inline", "css", "html" },
 
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = false,
 
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
+            -- Automatically install missing parsers when entering buffer
+            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            auto_install = true,
 
-        -- List of parsers to ignore installing (for "all")
-        --  ignore_install = { "javascript" },
+            -- List of parsers to ignore installing (for "all")
+            --  ignore_install = { "javascript" },
 
-        ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-        -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+            ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+            -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-        highlight = {
-            -- `false` will disable the whole extension
-            enable = true,
-        },
-        indent = {
-            enable = true
-        }
+            highlight = {
+                -- `false` will disable the whole extension
+                enable = true,
+            },
+            indent = {
+                enable = true
+            }
         }
     end
 }
