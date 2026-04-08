c48dc9d11aba04c9b1a623d6d3660ca0b46f78b1
Kyrill Gobber <kyrill.gobber@sharedien.com>
2026-04-08 08:32:50 +0200

    treesitter for nvim 0.12+

--- config/.config/nvim/lua/plugin_config/treesitter.lua
+++ config/.config/nvim/lua/plugin_config/treesitter.lua
@@ -1,31 +1,18 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
     config = function()
-        require 'nvim-treesitter.configs'.setup {
-            -- A list of parser names, or "all" (the four listed parsers should always be installed)
-            ensure_installed = { "c", "lua", "typescript", "tsx", "javascript", "http", "json", "vim", "markdown", "markdown_inline", "css", "html" },
+        -- Install parsers
+        require("nvim-treesitter").install({ "lua", "typescript", "markdown", "markdown_inline" })
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            -- List of parsers to ignore installing (for "all")
-            --  ignore_install = { "javascript" },
-
-            ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-            -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
-            },
-            indent = {
-                enable = true
-            }
-        }
-    end
+        -- Enable highlighting manually (this is now required!)
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "*",
+            callback = function()
+                pcall(vim.treesitter.start)
+            end,
+        })
+    end,
 }
